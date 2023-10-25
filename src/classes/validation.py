53a7e9fabe5aade028af83076f442a977b49dbe6
skeesh24 <d.pacooloff@gmail.com>
2023-10-25 22:59:37 +0500

    validations for statistic

--- src/classes/validation.py
+++ src/classes/validation.py
@@ -1,4 +1,5 @@
-from datetime import datetime
+from datetime import datetime, timedelta
+from typing import Tuple
 
 from pydantic import BaseModel
 
@@ -10,12 +11,40 @@ class service_model(BaseModel):
     service: str
     status: str
     description: str = ""
-    date_time: datetime = datetime.now()
+    date_time: datetime
 
     @staticmethod
     def create_as_dict(service: str, status: str, description: str = "") -> dict:
         representation = service_model(
-            service=service, status=status, description=description
+            service=service,
+            status=status,
+            description=description,
+            date_time=datetime.now(),
         ).model_dump()
         representation.pop(env.PARAM_ID)
         return representation
+
+
+class sla_object(BaseModel):
+    uptime: float
+    availability: float
+
+
+class difference_model(BaseModel):
+    service: str
+    status: str
+    date_time: datetime = datetime.now()
+    difference: timedelta = None
+
+
+class service_statistic(BaseModel):
+    enable_time: float
+    disabled_time: float
+    unstable_time: float
+    SLA: sla_object
+
+
+class service_status(BaseModel):
+    service: str
+    interval: Tuple[datetime, datetime]
+    statistic: service_statistic
